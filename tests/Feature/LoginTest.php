68f776faf2cd8189eec978f014ff94b866b8e135
Uzzair Baharudin <uzzaircode@gmail.com>
2019-05-19 11:16:02 +0800

    add correct response after successful login

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -2,32 +2,55 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Sailcast\User;
-
+use Tests\TestCase;
 
 class LoginTest extends TestCase
 {
     use RefreshDatabase;
 
-    
-    
-    public function test_user_will_receives_correct_message_upon_login_invalid_credentials(){
+    public function __construct()
+    {
 
-        $user = factory(User::class)->create();
+        $this->newUser = factory(User::class)->create();
 
-        $this->postJson('/login',[
+    }
 
-            'email' => $user->email,
-            'password' => 'wrongPassword'
+    /** @test */
+
+    public function correct_response_returned_after_successful_login()
+    {
+
+        $this->postJson('/login', [
+
+            'email' => $this->newUser->email,
+            'password' => 'secret',
 
         ])
-        ->assertStatus(422)
-        ->assertJson([
-            'message' => 'These credentials do not match our records'
-        ]);
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => 'OK',
+            ])
+            ->assertSessionHas('success');
+
+    }
+
+    /** @test */
+
+    public function user_will_receives_correct_message_upon_login_invalid_credentials()
+    {
+
+        $this->postJson('/login', [
+
+            'email' => $this->newUser->email,
+            'password' => 'wrongPassword',
+
+        ])
+            ->assertStatus(422)
+            ->assertJson([
+                'message' => 'These credentials do not match our records',
+            ]);
     }
 
 }
