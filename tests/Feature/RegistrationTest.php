ff1a5e1ecb4f19b8c46ee299d20a15f919ecfe1c
Uzzair Baharudin <uzzaircode@gmail.com>
2019-05-23 00:46:25 +0800

    wip: send email after new user registration

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -5,12 +5,22 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Mail;
 
 class RegistrationTest extends TestCase
 {
     /** @test */
 
     use RefreshDatabase;
+
+    /** @test */
+
+    public function guest_should_be_able_to_see_registration_page(){
+
+        $this->get('register')->assertStatus(200)->assertSee('Register');
+
+    }
+
 
     public function generate_username_upon_registration(){
 
@@ -30,4 +40,26 @@ class RegistrationTest extends TestCase
             'username' => str_slug('Uzzair Baharudin')
         ]);
     }
+
+    /**
+     * @test
+     */
+    
+     public function send_email_to_new_user_upon_registration(){
+
+
+        Mail::fake();
+        
+        $this->post('register', [
+
+            'name' => 'Uzzair Baharudin',
+            'email' => 'uzzairwebstudio@gmail.com',
+            
+            'password' => 'secret'
+
+        ])
+        ->assertRedirect();
+
+
+     }
 }
